d2bacb9ab28b04597517cbe1f15e84bd99a7f4af
Amir Rezvani <106575880+AREZVANI95@users.noreply.github.com>
2022-07-24 23:29:21 -0400

    Js update.
    Added Variables for what the app will display to the user after the user inputs a location.
    Added Variable for the Input and Buttons.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//Variable for the Search button
+var searchBtn = document.getElementById("search-button");
+//Variable for the Input field
+var searchBar = document.getElementById("input-form");
+//Variables for each category to be displayed
+var city = document.querySelector('.location')
+var temp = document.querySelector('.temperature')
+var wind = document.querySelector('.wind')
+var hum = document.querySelector('.humidity')
+var desc = document.querySelector('.description')
+var img = document.querySelector('.image')
