a1de240bb4cf3932c220aa80f618207fc5389218
Amir Rezvani <106575880+AREZVANI95@users.noreply.github.com>
2022-07-25 00:08:40 -0400

    Update script.js
    Added a Event on button click.
    Added a fetch function for the api.
    Added variables for the output after calling our api.
    Added a catch for when something other than a city is inputed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,28 @@ var wind = document.querySelector('.wind')
 var hum = document.querySelector('.humidity')
 var desc = document.querySelector('.description')
 var img = document.querySelector('.image')
+
+//Function for the search button to find the information for which the user inputted.
+searchBtn.addEventListener('click', function () {
+    //Fetch city from the weather api
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + searchBar.value + '&appid=fabd207c617e8fbed311e9e8c0946ea9')
+        .then(response => response.json())
+        .then(data => {
+            var nameValue = data['name'];
+            var tempValue = data['main']['temp'];
+            var windValue = data['wind']['speed'];
+            var humValue = data['main']['humidity'];
+            var descValue = data['weather'][0]['description'];
+            var imgValue = data['weather'][0]['icon'];
+
+            //Will display to the user in form of a list.
+            city.innerHTML = "City: " + nameValue;
+            temp.innerHTML = "Degree in Kelvin: " + tempValue;
+            wind.innerHTML = "Wind Speeds: " + windValue;
+            hum.innerHTML = "Humidity: " + humValue;
+            desc.innerHTML = "Weather Condition: " + descValue;
+            img.innerHTML = imgValue;
+        })
+//Catch if user inputs something invalid.
+    .catch(err => alert("Wrong city name!"))
+})
